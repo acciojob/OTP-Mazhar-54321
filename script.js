c06045d72ded91c8ee58dd229a7f1adf83029f02
Mazhar-54321 <160312563+Mazhar-54321@users.noreply.github.com>
2025-05-10 15:27:55 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,43 @@
 //your JS code here. If required.
+const inputs = document.querySelectorAll('.code');
+
+inputs.forEach((input, index) => {
+  input.addEventListener('input', (e) => {
+    const value = e.target.value;
+    if (value.match(/[0-9]/)) {
+      if (index < inputs.length - 1) {
+        inputs[index + 1].focus();
+      }
+    } else {
+      e.target.value = ''; // Clear non-digit input
+    }
+  });
+
+  input.addEventListener('keydown', (e) => {
+    if (e.key === 'Backspace') {
+      if (input.value === '' && index > 0) {
+        inputs[index - 1].focus();
+      }
+    }
+  });
+
+  input.addEventListener('paste', (e) => {
+    e.preventDefault();
+    const pasteData = e.clipboardData.getData('text').replace(/\D/g, '');
+    pasteData.split('').forEach((char, i) => {
+      if (inputs[i]) {
+        inputs[i].value = char;
+      }
+    });
+    if (pasteData.length >= 6) {
+      inputs[5].focus();
+    } else {
+      inputs[pasteData.length].focus();
+    }
+  });
+});
+
+// Auto-focus first input on load
+window.addEventListener('load', () => {
+  inputs[0].focus();
+});
